265be5bab8f02022ba416bb2faa903ced1b9f8ed
Chi Hoang <Chi_Hoang@intuit.com>
2019-09-24 22:54:34 -0700

    Initial Spring Boot and mvnw setup

---
+++ src/main/java/com/twitter/client/TwitterClientApplication.java
@@ -0,0 +1,13 @@
+package com.twitter.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterClientApplication.class, args);
+	}
+
+}
