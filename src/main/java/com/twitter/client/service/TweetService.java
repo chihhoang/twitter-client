1f590f1978c04f90d80c825a49a72810b74d6616
anvitha <54367501+anvithakp@users.noreply.github.com>
2019-10-20 17:18:50 -0700

    Update TweetService.java

--- src/main/java/com/twitter/client/service/TweetService.java
+++ src/main/java/com/twitter/client/service/TweetService.java
@@ -7,6 +7,10 @@ import twitter4j.Status;
 import twitter4j.TwitterException;
 
 /** @author choang */
+/***
+ * @author Anvitha Karanam
+ * Added a new searchTwitter method signature 
+ ***/
 public interface TweetService {
   Status createTweet(String text) throws TwitterException;
 
